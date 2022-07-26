0df0bae37491d9dede2d4b2b8099820bf53cfaa6
Stephen <galea.steph@gmail.com>
2022-07-26 16:31:44 +0100

    Finished commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="price-container">
+        <div className="full-width-col"> 
+          <h1>Join our community</h1>         
+          <h2>30-day, hassle-free money back guarantee</h2>
+          <p>
+            Gain access to our full library of tutorials along with expert code reviews. 
+            Perfect for any developers who are serious about honing their skills.
+          </p>
+        </div>
+        <div className="subs-col">
+          <h2>Monthly Subscription</h2>  
+          <h3>$29 <span>per month</span></h3> 
+          <p>
+            Full access for less than $1 a day 
+          </p>   
+          <button>Sign Up</button>              
+        </div>  
+        <div className="why-us-col">
+          <h2>Why Us</h2> 
+          <p>
+            Tutorials by industry experts <br></br>
+            Peer & expert code review <br></br>
+            Coding exercises <br></br>
+            Access to our GitHub repos <br></br>
+            Community forum <br></br>
+            Flashcard decks <br></br>
+            New videos every week <br></br>
+          </p>
+        </div>    
+      </div>
     </div>
   );
 }
